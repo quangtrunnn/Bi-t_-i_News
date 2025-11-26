e1128c1c1d842b0d54897f67081892411b0451ca
quangtrunnn <89956511+quangtrunnn@users.noreply.github.com>
2025-11-26 18:52:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ RSS_SOURCES = [
 
     # --- 15 NGUỒN MỚI TỪ NGƯỜI QUAN SÁT (BỎ ĐUÔI .RSS) ---
     # 1. Chứng Khoán (4 nguồn)
+    "https://nguoiquansat.vn/rss/trang-chu.rss",
     "https://nguoiquansat.vn/rss/chung-khoan", 
     "https://nguoiquansat.vn/rss/chung-khoan/chuyen-dong-thi-truong", 
     "https://nguoiquansat.vn/rss/chung-khoan/doanh-nghiep-az", 
